68a03fc3e504f8149b0030db0a94329d6a6ac5f8
TamaraKatthain <katthaint@gmail.com>
2026-04-13 14:55:14 -0700

    Update page.jsx

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,32 +1,55 @@
 export default function Projects() {
   return (
     <main className="app-container">
-      <h1 className="text-2xl font-bold mb-6">Projects</h1>
 
-      <h2 className="text-xl font-semibold mb-4">Writing</h2>
-      <ul className="mb-6 space-y-2">
+      <h1 className="text-3xl font-bold mb-8">
+        Projects
+      </h1>
+
+      <ul className="space-y-6 max-w-2xl">
+
         <li>
-          <a href="https://medium.com/@TamaraKatthain" target="_blank">
-            Medium
+          <a 
+            href="https://canva.link/1ew0xt5tb15ckxw" 
+            target="_blank"
+            className="underline"
+          >
+            Nike Quant / Qual Research
           </a>
         </li>
+
         <li>
-          <a href="https://substack.com/@tamarakatthain" target="_blank">
-            Substack
+          <a 
+            href="https://canva.link/94azntsqgbb1ht6" 
+            target="_blank"
+            className="underline"
+          >
+            Nike Campaign Analysis
           </a>
         </li>
+
         <li>
-          <a href="https://www.uscannenbergmedia.com/people/tamara-katthain/" target="_blank">
-            USC News Columnist
+          <a 
+            href="https://canva.link/pri9jn8a4t7jaev" 
+            target="_blank"
+            className="underline"
+          >
+            Pedidos Ya
           </a>
         </li>
+
+        <li>
+          <a 
+            href="https://canva.link/ydlkeupkn4ah7sv" 
+            target="_blank"
+            className="underline"
+          >
+            Red Bull
+          </a>
+        </li>
+
       </ul>
 
-      <h2 className="text-xl font-semibold mb-4">Advertising Projects</h2>
-      <ul className="space-y-2">
-        <li>P1 Skincare</li>
-        <li>Nike</li>
-      </ul>
     </main>
   )
 }
