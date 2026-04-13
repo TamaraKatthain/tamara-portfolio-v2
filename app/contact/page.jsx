dd4346bde6b9b72ecc7c3a255b2b76efd67145d9
TamaraKatthain <katthaint@gmail.com>
2026-04-13 14:55:44 -0700

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,21 +1,33 @@
 export default function Contact() {
   return (
     <main className="app-container">
-      <h1 className="text-2xl font-bold mb-6">Contact</h1>
 
-      <div className="mb-6">
-        <p className="font-semibold">Email:</p>
-        <a href="mailto:katthaint@gmail.com">
-          katthaint@gmail.com
-        </a>
+      <h1 className="text-3xl font-bold mb-8">
+        Contact
+      </h1>
+
+      <div className="space-y-6 max-w-xl">
+
+        <div>
+          <p className="text-sm text-gray-700">Email</p>
+          <a href="mailto:katthaint@gmail.com" className="underline">
+            katthaint@gmail.com
+          </a>
+        </div>
+
+        <div>
+          <p className="text-sm text-gray-700">LinkedIn</p>
+          <a 
+            href="https://www.linkedin.com/in/tamara-katthain" 
+            target="_blank"
+            className="underline"
+          >
+            www.linkedin.com/in/tamara-katthain
+          </a>
+        </div>
+
       </div>
 
-      <div>
-        <p className="font-semibold">LinkedIn:</p>
-        <a href="https://www.linkedin.com/in/tamara-katthain" target="_blank">
-          www.linkedin.com/in/tamara-katthain
-        </a>
-      </div>
     </main>
   )
 }
