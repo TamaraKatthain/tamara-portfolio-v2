be4fb72d34a7ed7a972129b7714bbb479149d047
TamaraKatthain <katthaint@gmail.com>
2026-04-13 13:50:07 -0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="app-container">
+      <h1 className="text-3xl font-bold mb-6">
+        I constantly question the world around me in big (this is my brand principle).
+      </h1>
+
+      <p className="text-lg font-semibold">
+        As an insights strategist, I uncover what people truly think about and need from brands, and use those insights to create meaningful connections.
+      </p>
+    </main>
+  )
+}
