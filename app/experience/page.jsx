903a32438407eea5d9d710cefa73617e42ea7f5e
TamaraKatthain <katthaint@gmail.com>
2026-04-13 14:54:40 -0700

    Update page.jsx

--- app/experience/page.jsx
+++ app/experience/page.jsx
@@ -1,17 +1,19 @@
 export default function Experience() {
   return (
     <main className="app-container">
-      <h1 className="text-2xl font-bold mb-6">
-        Experience (Support / Reason to Believe)
+
+      <h1 className="text-3xl font-bold mb-8">
+        Experience
       </h1>
 
-      <ul className="space-y-4">
+      <ul className="list-disc pl-6 space-y-4 max-w-2xl">
         <li>3+ years of advertising experience across agency and client-side roles.</li>
         <li>Experience working with legendary clients like Tinder, Walmart, Visa, and OceanSpray.</li>
         <li>Supported the launch of 20+ products to market across CPG categories.</li>
         <li>Boston University BA in Psychology, Minor in Advertising.</li>
         <li>USC MS in DSM, a highly selective program shaping the future of communication.</li>
       </ul>
+
     </main>
   )
 }
