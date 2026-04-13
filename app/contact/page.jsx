77e2b8a1220549921a5e7f66c88d752f335efc7e
TamaraKatthain <katthaint@gmail.com>
2026-04-13 14:01:59 -0700

    Create page.jsx

---
+++ app/contact/page.jsx
@@ -0,0 +1,21 @@
+export default function Contact() {
+  return (
+    <main className="app-container">
+      <h1 className="text-2xl font-bold mb-6">Contact</h1>
+
+      <div className="mb-6">
+        <p className="font-semibold">Email:</p>
+        <a href="mailto:katthaint@gmail.com">
+          katthaint@gmail.com
+        </a>
+      </div>
+
+      <div>
+        <p className="font-semibold">LinkedIn:</p>
+        <a href="https://www.linkedin.com/in/tamara-katthain" target="_blank">
+          www.linkedin.com/in/tamara-katthain
+        </a>
+      </div>
+    </main>
+  )
+}
