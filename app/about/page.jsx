7f15bec0cd9f43db84e177f9387685bc438dc55f
TamaraKatthain <katthaint@gmail.com>
2026-04-13 18:09:44 -0700

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -16,7 +16,7 @@ export default function About() {
           </h3>
 
           <p className="text-base font-semibold max-w-xs leading-relaxed">
-            At the core, I believe people want to feel understood. Not targeted, not sold to, but seen. The strongest brands don’t just communicate, they resonate.
+            Curiosity and exploration drive everything I do. I am constantly observing, questioning, and trying to understand why people think and act the way they do.
           </p>
 
         </div>
@@ -36,7 +36,7 @@ export default function About() {
           </h3>
 
           <p className="text-base leading-relaxed">
-            I am drawn to brands that care deeply about their audience and are not afraid to explore nuance. The ones willing to ask better questions, challenge assumptions, and create work that reflects real human behavior, not just trends.
+            The kind of person who enjoys sitting down at a coffee shop and people watching for hours. Someone who is obsessed with analyzing human behavior, especially consumer purchasing behavior, and finding patterns in what people like, what they buy, and what they are talking about.
           </p>
 
         </div>
@@ -56,7 +56,7 @@ export default function About() {
           </h3>
 
           <p className="text-base leading-relaxed">
-            I want clients to feel clarity. To feel like their audience finally makes sense. My goal is to turn complexity into direction, and insights into something that actually moves people and drives meaningful connection.
+            When I work with Tamara, it feels like we could brainstorm for hours. She makes me feel confident in speaking up and sharing my ideas in a judgment-free space. We analyze everything in depth, as we are both completely obsessed with understanding human behavior and the underlying reasons behind our actions.
           </p>
 
         </div>
