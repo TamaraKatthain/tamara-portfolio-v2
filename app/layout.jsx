7175f73e3982916a2016280798cdd37e278c5a58
TamaraKatthain <katthaint@gmail.com>
2026-04-13 17:41:23 -0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#2F8EDC] text-white`}>
+      <body className={`${inter.className} bg-[#EAE6DF] text-black`}>
         <Nav />
         {children}
       </body>
