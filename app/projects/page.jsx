7a9dd4554906f7cd785b6dd757cdc9797a2387b8
TamaraKatthain <katthaint@gmail.com>
2026-04-14 14:14:37 -0700

    Update page.jsx

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -5,39 +5,50 @@ export default function Projects() {
       {/* HEADLINE */}
       <div className="mb-24">
         <h1 className="text-7xl md:text-8xl font-extrabold text-[#2F8EDC]">
-          What it looks like in practice.
+          What it looks like in practice
         </h1>
       </div>
 
-      {/* WORK GRID */}
       <section className="flex justify-center gap-24">
 
         {/* NIKE */}
-        <div className="text-center group">
+        <a
+          href="https://canva.link/94azntsqgbb1ht6"
+          target="_blank"
+          className="text-center group"
+        >
           <h3 className="text-[#2F8EDC] font-bold mb-4">NIKE</h3>
           <img
             src="/Nike.png"
             className="w-[350px] h-[220px] object-cover grayscale group-hover:grayscale-0 transition duration-500"
           />
-        </div>
+        </a>
 
         {/* PEDIDOS YA */}
-        <div className="text-center group">
+        <a
+          href="https://canva.link/pri9jn8a4t7jaev"
+          target="_blank"
+          className="text-center group"
+        >
           <h3 className="text-[#2F8EDC] font-bold mb-4">PEDIDOS YA</h3>
           <img
             src="/Pedidosya.png"
             className="w-[350px] h-[220px] object-cover grayscale group-hover:grayscale-0 transition duration-500"
           />
-        </div>
+        </a>
 
         {/* RED BULL */}
-        <div className="text-center group">
+        <a
+          href="https://canva.link/ydlkeupkn4ah7sv"
+          target="_blank"
+          className="text-center group"
+        >
           <h3 className="text-[#2F8EDC] font-bold mb-4">RED BULL</h3>
           <img
             src="/Redbull.png"
             className="w-[350px] h-[220px] object-cover grayscale group-hover:grayscale-0 transition duration-500"
           />
-        </div>
+        </a>
 
       </section>
 
