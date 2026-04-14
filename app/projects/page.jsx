721054ca4ac9ff11accb278ac1c6a330a2b5dc95
TamaraKatthain <katthaint@gmail.com>
2026-04-13 17:57:35 -0700

    Update page.jsx

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -4,46 +4,46 @@ export default function Projects() {
   return (
     <main className="app-container">
 
-      <section className="flex gap-16 justify-center">
+      <section className="flex gap-20 justify-center items-start">
 
         {/* Nike */}
-        <div className="space-y-3">
-          <p className="text-lg font-semibold text-[#2F8EDC]">NIKE</p>
-          <div className="w-[300px] h-[400px] overflow-hidden">
+        <div className="w-[320px]">
+          <p className="text-lg font-semibold text-[#2F8EDC] mb-3">NIKE</p>
+
+          <div className="relative w-full h-[400px]">
             <Image
               src="/Nike.png"
               alt="Nike"
-              width={300}
-              height={400}
-              className="w-full h-full object-cover"
+              fill
+              className="object-cover"
             />
           </div>
         </div>
 
         {/* Pedidos Ya */}
-        <div className="space-y-3">
-          <p className="text-lg font-semibold text-[#2F8EDC]">PEDIDOS YA</p>
-          <div className="w-[300px] h-[400px] overflow-hidden">
+        <div className="w-[320px]">
+          <p className="text-lg font-semibold text-[#2F8EDC] mb-3">PEDIDOS YA</p>
+
+          <div className="relative w-full h-[400px]">
             <Image
               src="/Pedidosya.png"
               alt="Pedidos"
-              width={300}
-              height={400}
-              className="w-full h-full object-cover"
+              fill
+              className="object-cover"
             />
           </div>
         </div>
 
         {/* Red Bull */}
-        <div className="space-y-3">
-          <p className="text-lg font-semibold text-[#2F8EDC]">RED BULL</p>
-          <div className="w-[300px] h-[400px] overflow-hidden">
+        <div className="w-[320px]">
+          <p className="text-lg font-semibold text-[#2F8EDC] mb-3">RED BULL</p>
+
+          <div className="relative w-full h-[400px]">
             <Image
               src="/Redbull.png"
               alt="Red Bull"
-              width={300}
-              height={400}
-              className="w-full h-full object-cover"
+              fill
+              className="object-cover"
             />
           </div>
         </div>
