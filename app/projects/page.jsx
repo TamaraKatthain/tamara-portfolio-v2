978f08b3346fee60afa42bf88b5f7f58247a0e38
TamaraKatthain <katthaint@gmail.com>
2026-04-14 14:00:52 -0700

    Update page.jsx

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -13,29 +13,29 @@ export default function Projects() {
       <section className="flex justify-center gap-24">
 
         {/* NIKE */}
-        <div className="text-center">
+        <div className="text-center group">
           <h3 className="text-[#2F8EDC] font-bold mb-4">NIKE</h3>
           <img
             src="/Nike.png"
-            className="w-[350px] h-[220px] object-cover grayscale"
+            className="w-[350px] h-[220px] object-cover grayscale group-hover:grayscale-0 transition duration-500"
           />
         </div>
 
         {/* PEDIDOS YA */}
-        <div className="text-center">
+        <div className="text-center group">
           <h3 className="text-[#2F8EDC] font-bold mb-4">PEDIDOS YA</h3>
           <img
             src="/Pedidosya.png"
-            className="w-[350px] h-[220px] object-cover grayscale"
+            className="w-[350px] h-[220px] object-cover grayscale group-hover:grayscale-0 transition duration-500"
           />
         </div>
 
         {/* RED BULL */}
-        <div className="text-center">
+        <div className="text-center group">
           <h3 className="text-[#2F8EDC] font-bold mb-4">RED BULL</h3>
           <img
             src="/Redbull.png"
-            className="w-[350px] h-[220px] object-cover grayscale"
+            className="w-[350px] h-[220px] object-cover grayscale group-hover:grayscale-0 transition duration-500"
           />
         </div>
 
