5cfd2ffd175ff73a45a210dde381a24b7fff8800
TamaraKatthain <katthaint@gmail.com>
2026-04-13 17:52:21 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,39 +1,61 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function Projects() {
   return (
-    <main className="app-container min-h-screen">
+    <main className="app-container">
 
-      <section className="grid md:grid-cols-2 gap-20 items-center">
+      {/* HORIZONTAL GALLERY */}
+      <section className="flex gap-10 overflow-x-auto">
 
-        {/* LEFT — TEXT */}
-        <div className="space-y-8">
-
-          <h1 className="text-7xl font-extrabold leading-[0.95]">
-            Tamara
-            <br />
-            Katthain
-          </h1>
-
-          <h2 className="text-3xl max-w-lg leading-snug">
-            I constantly question the world around me
-          </h2>
-
-          <p className="text-lg max-w-md">
-            As an insights strategist, I uncover the quiet truths behind what people say and what they need from brands, using those insights to create meaningful connections.
+        {/* Nike */}
+        <div className="min-w-[300px] space-y-3">
+          <p className="text-lg font-semibold text-[#2F8EDC]">
+            NIKE
           </p>
 
+          <a href="https://canva.link/94azntsqgbb1ht6" target="_blank">
+            <Image
+              src="/Nike.png"
+              alt="Nike"
+              width={300}
+              height={400}
+              className="object-cover"
+            />
+          </a>
         </div>
 
-        {/* RIGHT — IMAGE */}
-        <div className="flex justify-end">
-          <Image
-            src="/final.jpeg"
-            alt="Tamara Katthain"
-            width={400}
-            height={500}
-            className="object-cover"
-          />
+        {/* Pedidos Ya */}
+        <div className="min-w-[300px] space-y-3">
+          <p className="text-lg font-semibold text-[#2F8EDC]">
+            PEDIDOS YA
+          </p>
+
+          <a href="https://canva.link/pri9jn8a4t7jaev" target="_blank">
+            <Image
+              src="/Pedidosya.png"
+              alt="Pedidos"
+              width={300}
+              height={400}
+              className="object-cover"
+            />
+          </a>
+        </div>
+
+        {/* Red Bull */}
+        <div className="min-w-[300px] space-y-3">
+          <p className="text-lg font-semibold text-[#2F8EDC]">
+            RED BULL
+          </p>
+
+          <a href="https://canva.link/ydlkeupkn4ah7sv" target="_blank">
+            <Image
+              src="/Redbull.png"
+              alt="Red Bull"
+              width={300}
+              height={400}
+              className="object-cover"
+            />
+          </a>
         </div>
 
       </section>
