d9306da98a0f897645515dbdec17867390b35a39
TamaraKatthain <katthaint@gmail.com>
2026-04-13 17:22:15 -0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Nav from './components/Nav'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+
+        <Nav />
+
         {children}
+
       </body>
     </html>
   )
